15d0e0d6bbfe434bae857c7a7489aed642b5739d
Federico Zivolo <federico.zivolo@gmail.com>
2018-10-30 11:08:54 +0100

    feat: add optional --analyze flag requirement to generate report

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 const curry = require("lodash.curry");
 const flow = require("lodash.flow");
 
-const addBundleVisualizer = (options = {}) => config => {
+const addBundleVisualizer = (options = {}, behindFlag = false) => config => {
   const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
     .BundleAnalyzerPlugin;
 
-  config.plugins.push(
-    new BundleAnalyzerPlugin(
-      Object.assign(
-        {
-          analyzerMode: "static",
-          reportFilename: "report.html"
-        },
-        options
+  // if behindFlag is set to true, the report will be created only if
+  // the `--analyze` flag is added to the `yarn build` command
+  if (behindFlag ? process.argv.includes('--analyze') : true) {
+    config.plugins.push(
+      new BundleAnalyzerPlugin(
+        Object.assign(
+          {
+            analyzerMode: "static",
+            reportFilename: "report.html"
+          },
+          options
+        )
       )
-    )
-  );
+    );
+  }
   return config;
 };
 
