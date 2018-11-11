82fea96c11713c54c6f026ba1b7aa7a8f3f07e43
Andrej Naumovski <a.naumovski996@yahoo.com>
2018-11-11 17:38:07 +0100

    feat: Add enableEslintTypescript helper.
    * This helper modifies the ESLint loader to process .ts and .tsx files as well.

--- index.js
+++ index.js
@@ -103,6 +103,24 @@ const useEslintRc = () => config => {
   return config;
 };
 
+const enableEslintTypescript = () => config => {
+  const eslintRule = config.module.rules.filter(
+    r => r.use && r.use.some(u => u.options && u.options.useEslintrc !== void 0)
+  )[0];
+
+  eslintRule.test = /\.([j,t]sx?|mjs)$/;
+
+  const rules = config.module.rules.map(
+    r =>
+      r.use && r.use.some(u => u.options && u.options.useEslintrc !== void 0)
+        ? eslintRule
+        : r
+  );
+  config.module.rules = rules;
+
+  return config;
+}
+
 const useBabelRc = () => config => {
   const babelLoaderFilter = rule =>
     rule.loader &&
@@ -155,6 +173,7 @@ module.exports = {
   addWebpackAlias,
   adjustWorkbox,
   useEslintRc,
+  enableEslintTypescript,
   addBabelPlugins,
   fixBabelImports,
   useBabelRc
