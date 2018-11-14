3fe9c452a1443a61edcaa9bcc5ba9952d90bda57
Norn <nlfctx@outlook.com>
2018-11-14 16:14:15 +0800

    feat: enable less-loader

--- index.js
+++ index.js
@@ -164,6 +164,68 @@ const fixBabelImports = (libraryName, options) =>
     `fix-${libraryName}-imports`
   ]);
 
+const addLessLoader = (loaderOptions = {}) => config => {
+  const mode = process.env.NODE_ENV === 'development' ? 'dev' : 'prod';
+
+  // Need these for production mode, which are copied from react-scripts
+  const publicPath = require('react-scripts/config/paths').servedPath;
+  const shouldUseRelativeAssetPaths = publicPath === './';
+  const shouldUseSourceMap
+    = mode === 'prod' && process.env.GENERATE_SOURCEMAP !== 'false';
+
+  const lessLoader = [
+    mode === 'dev'
+      ? require.resolve('style-loader')
+      : {
+        loader: require('mini-css-extract-plugin').loader,
+        options: Object.assign(
+          {},
+          shouldUseRelativeAssetPaths
+            ? { publicPath: '../../' }
+            : undefined
+        )
+      },
+    {
+      loader: require.resolve('css-loader'),
+      options: { importLoaders: 2 }
+    },
+    {
+      loader: require.resolve('postcss-loader'),
+      options: {
+        ident: 'postcss',
+        plugins: () => [
+          require('postcss-flexbugs-fixes'),
+          require('postcss-preset-env')({
+            autoprefixer: {
+              flexbox: 'no-2009'
+            },
+            stage: 3
+          })
+        ],
+        sourceMap: shouldUseSourceMap
+      }
+    },
+    {
+      loader: require.resolve('less-loader'),
+      options: Object.assign(loaderOptions, {
+        source: shouldUseSourceMap
+      })
+    }
+  ];
+
+  const loaders = config.module.rules.find(rule => Array.isArray(rule.oneOf))
+    .oneOf;
+
+  // Insert less-loader as the penultimate item of loaders (before file-loader)
+  loaders.splice(loaders.length - 2, 0, {
+    test: /\.less$/,
+    use: lessLoader
+  });
+
+  return config;
+};
+
+
 module.exports = {
   override,
   addBundleVisualizer,
@@ -176,5 +238,6 @@ module.exports = {
   enableEslintTypescript,
   addBabelPlugins,
   fixBabelImports,
-  useBabelRc
+  useBabelRc,
+  addLessLoader
 };
