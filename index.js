1ca6825d93cc9b0cfdae9f0c49e096a19dc3523f
Kevin Colten <kevincolten@gmail.com>
2018-11-22 23:19:08 -0600

    added babel include

--- index.js
+++ index.js
@@ -23,12 +23,12 @@ const addBundleVisualizer = (options = {}, behindFlag = false) => config => {
   return config;
 };
 
-const addBabelPlugin = plugin => config => {
+const getBabelLoader = config => {
   const babelLoaderFilter = rule =>
-    rule.loader &&
-    rule.loader.includes("babel") &&
-    rule.options &&
-    rule.options.plugins;
+  rule.loader &&
+  rule.loader.includes("babel") &&
+  rule.options &&
+  rule.options.plugins;
 
   // First, try to find the babel loader inside the oneOf array.
   // This is where we can find it when working with react-scripts@2.0.3.
@@ -43,9 +43,11 @@ const addBabelPlugin = plugin => config => {
     loaders = loaders.reduce((ldrs, rule) => ldrs.concat(rule.use || []), []);
     babelLoader = loaders.find(babelLoaderFilter);
   }
+  return babelLoader;
+}
 
-  babelLoader.options.plugins.push(plugin);
-
+const addBabelPlugin = plugin => config => {
+  getBabelLoader(config).options.plugins.push(plugin);
   return config;
 };
 
@@ -120,28 +122,12 @@ const enableEslintTypescript = () => config => {
 };
 
 const useBabelRc = () => config => {
-  const babelLoaderFilter = rule =>
-    rule.loader &&
-    rule.loader.includes("babel") &&
-    rule.options &&
-    rule.options.plugins;
+  getBabelLoader(config).options.babelrc = true;
+  return config;
+};
 
-  // First, try to find the babel loader inside the oneOf array.
-  // This is where we can find it when working with react-scripts@2.0.3.
-  let loaders = config.module.rules.find(rule => Array.isArray(rule.oneOf))
-    .oneOf;
-
-  let babelLoader = loaders.find(babelLoaderFilter);
-
-  // If the loader was not found, try to find it inside of the "use" array, within the rules.
-  // This should work when dealing with react-scripts@2.0.0.next.* versions.
-  if (!babelLoader) {
-    loaders = loaders.reduce((ldrs, rule) => ldrs.concat(rule.use || []), []);
-    babelLoader = loaders.find(babelLoaderFilter);
-  }
-
-  babelLoader.options.babelrc = true;
-
+const babelInclude = include => config => {
+  getBabelLoader(config).include = include;
   return config;
 };
 
@@ -258,5 +244,6 @@ module.exports = {
   useBabelRc,
   addLessLoader,
   overrideDevServer,
-  watchAll
+  watchAll,
+  babelInclude
 };
