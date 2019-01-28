2f394f617bfcdfe6d8139a8b6fbcb5d67f4234fc
Ling ZiQing <onlylingstudio@gmail.com>
2019-01-28 10:10:26 +0800

    add CSS Modules (#51)
    `.module.less` will use CSS Modules.
    we can custom `localIdentName`.
    
    ```javascript
    const {
        override,
        addLessLoader
    } = require('customize-cra');
    
    module.exports = override(
        addLessLoader({
            localIdentName: '[local]--[hash:base64:5]'
        })
    );
    ```

--- index.js
+++ index.js
@@ -165,53 +165,68 @@ const addLessLoader = (loaderOptions = {}) => config => {
   const shouldUseRelativeAssetPaths = publicPath === "./";
   const shouldUseSourceMap =
     mode === "prod" && process.env.GENERATE_SOURCEMAP !== "false";
+  const lessRegex = /\.less$/;
+  const lessModuleRegex = /\.module\.less$/;
+  const localIdentName = loaderOptions.localIdentName || "[path][name]__[local]--[hash:base64:5]";
 
-  const lessLoader = [
-    mode === "dev"
-      ? require.resolve("style-loader")
-      : {
-          loader: require("mini-css-extract-plugin").loader,
-          options: Object.assign(
-            {},
-            shouldUseRelativeAssetPaths ? { publicPath: "../../" } : undefined
-          )
-        },
-    {
-      loader: require.resolve("css-loader"),
-      options: { importLoaders: 2 }
-    },
-    {
-      loader: require.resolve("postcss-loader"),
-      options: {
-        ident: "postcss",
-        plugins: () => [
-          require("postcss-flexbugs-fixes"),
-          require("postcss-preset-env")({
-            autoprefixer: {
-              flexbox: "no-2009"
-            },
-            stage: 3
-          })
-        ],
-        sourceMap: shouldUseSourceMap
+  const getLessLoader = (cssOptions) => {
+    return [
+      mode === "dev"
+        ? require.resolve("style-loader")
+        : {
+            loader: require("mini-css-extract-plugin").loader,
+            options: Object.assign(
+              {},
+              shouldUseRelativeAssetPaths ? { publicPath: "../../" } : undefined
+            )
+          },
+      {
+        loader: require.resolve("css-loader"),
+        options: cssOptions
+      },
+      {
+        loader: require.resolve("postcss-loader"),
+        options: {
+          ident: "postcss",
+          plugins: () => [
+            require("postcss-flexbugs-fixes"),
+            require("postcss-preset-env")({
+              autoprefixer: {
+                flexbox: "no-2009"
+              },
+              stage: 3
+            })
+          ],
+          sourceMap: shouldUseSourceMap
+        }
+      },
+      {
+        loader: require.resolve("less-loader"),
+        options: Object.assign(loaderOptions, {
+          source: shouldUseSourceMap
+        })
       }
-    },
-    {
-      loader: require.resolve("less-loader"),
-      options: Object.assign(loaderOptions, {
-        source: shouldUseSourceMap
-      })
-    }
-  ];
+    ];
+  };
 
   const loaders = config.module.rules.find(rule => Array.isArray(rule.oneOf))
     .oneOf;
 
   // Insert less-loader as the penultimate item of loaders (before file-loader)
   loaders.splice(loaders.length - 1, 0, {
-    test: /\.less$/,
-    use: lessLoader,
+    test: lessRegex,
+    exclude: lessModuleRegex,
+    use: getLessLoader({
+      importLoaders: 2
+    }),
     sideEffects: mode === "prod"
+  }, {
+    test: lessModuleRegex,
+    use: getLessLoader({
+      importLoaders: 2,
+      modules: true,
+      localIdentName: localIdentName
+    })
   });
 
   return config;
