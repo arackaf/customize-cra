1b4eaccdb4e4c77bc913ac0335aed737354bb46f
Federico Zivolo <federico.zivolo@gmail.com>
2019-02-08 19:59:04 +0100

    feat: removeModuleScopePlugin (#55)

--- index.js
+++ index.js
@@ -284,6 +284,15 @@ const addPostcssPlugins = (plugins) => config => {
   return config;
 }
 
+// This will remove the CRA plugin that prevents to import modules from
+// outside the `src` directory, useful if you use a different directory
+const removeModuleScopePlugin = () => config => {
+  config.resolve.plugins = config.resolve.plugins.filter(
+    p => p.constructor.name !== "ModuleScopePlugin"
+  );
+  return config;
+};
+
 module.exports = {
   override,
   addBundleVisualizer,
@@ -305,5 +314,6 @@ module.exports = {
   addBabelPresets,
   disableChunk,
   addPostcssPlugins,
-  getBabelLoader
+  getBabelLoader,
+  removeModuleScopePlugin
 };
