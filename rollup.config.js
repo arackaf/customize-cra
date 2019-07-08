9e2cb011d2a22964055656e41b56e99d5c38e5ce
Mark Chandler <mark@chanchan.io>
2019-07-08 13:38:16 -0400

    chore(rollup): add babel plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+import babel from "rollup-plugin-babel";
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
 import pkg from "./package.json";
@@ -10,6 +11,12 @@ export default [
       { file: pkg.main, format: "cjs" },
       { file: pkg.module, format: "es" }
     ],
-    plugins: [resolve(), commonjs()]
+    plugins: [
+      resolve(),
+      commonjs(),
+      babel({
+        exclude: "node_modules/**"
+      })
+    ]
   }
 ];
