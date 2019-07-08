1ef5b78f16adf1a031ff3536cb4a184662e4c2ef
Mark Chandler <mark@chanchan.io>
2019-07-08 08:47:43 -0400

    chore(rollup): configure basic build process

---
+++ rollup.config.js
@@ -0,0 +1,15 @@
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import pkg from "./package.json";
+
+export default [
+  {
+    input: "src/index.js",
+    external: ["lodash.flow"],
+    output: [
+      { file: pkg.main, format: "cjs" },
+      { file: pkg.module, format: "es" }
+    ],
+    plugins: [resolve(), commonjs()]
+  }
+];
